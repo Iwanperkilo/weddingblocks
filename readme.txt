5318683559bd38f474a7c7377819c8afc3f6dc73
Iwanperkilo <iwanperkilo@users.noreply.github.com>
2026-07-20 12:50:50 +0700

    rilis versi 1.2.0 - tambah system animasi plugin weddingblocks

--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Tags: wedding, invitation, gutenberg, fse, blocks
 Requires at least: 6.7
 Tested up to: 7.0
 Requires PHP: 7.4
-Stable tag: 1.2.0-beta
+Stable tag: 1.2.0
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 Plugin URI: https://wordpress.org/plugins/weddingblocks/
--- weddingblocks.php
+++ weddingblocks.php
@@ -1,23 +1,24 @@
 <?php
+
 /**
  * Plugin Name: WeddingBlocks
  * Plugin URI: https://wordpress.org/plugins/weddingblocks/
  * Description: A digital invitation plugin based on Gutenberg & Full Site Editing (FSE) with a modern, interactive, and easily customizable design.
- * Version: 1.2.0-beta
+ * Version: 1.2.0
  * Author: Perkilo
  * Author URI: https://github.com/iwanperkilo
  * License: GPLv2 or later
  * Text Domain: weddingblocks
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
+if (! defined('ABSPATH')) {
     exit; // Exit if accessed directly.
 }
 
 // Define Constants.
-define( 'WEDDINGBLOCKS_VERSION', '1.2.0-beta' );
-define( 'WEDDINGBLOCKS_PATH', plugin_dir_path( __FILE__ ) );
-define( 'WEDDINGBLOCKS_URL', plugin_dir_url( __FILE__ ) );
+define('WEDDINGBLOCKS_VERSION', '1.2.0');
+define('WEDDINGBLOCKS_PATH', plugin_dir_path(__FILE__));
+define('WEDDINGBLOCKS_URL', plugin_dir_url(__FILE__));
 
 // Load Sub-modules.
 require_once WEDDINGBLOCKS_PATH . 'includes/helpers.php';
@@ -28,22 +29,24 @@ require_once WEDDINGBLOCKS_PATH . 'includes/admin-page.php';
 require_once WEDDINGBLOCKS_PATH . 'includes/blocks.php';
 
 // Plugin Activation.
-register_activation_hook( __FILE__, 'weddingblocks_activate' );
-function weddingblocks_activate() {
+register_activation_hook(__FILE__, 'weddingblocks_activate');
+function weddingblocks_activate()
+{
     // Install or update database.
-    if ( function_exists( 'weddingblocks_db_install' ) ) {
+    if (function_exists('weddingblocks_db_install')) {
         weddingblocks_db_install();
     }
-    
+
     // Flush rewrite rules for CPT.
-    if ( function_exists( 'weddingblocks_register_cpt' ) ) {
+    if (function_exists('weddingblocks_register_cpt')) {
         weddingblocks_register_cpt();
     }
     flush_rewrite_rules();
 }
 
 // Plugin Deactivation.
-register_deactivation_hook( __FILE__, 'weddingblocks_deactivate' );
-function weddingblocks_deactivate() {
+register_deactivation_hook(__FILE__, 'weddingblocks_deactivate');
+function weddingblocks_deactivate()
+{
     flush_rewrite_rules();
 }
